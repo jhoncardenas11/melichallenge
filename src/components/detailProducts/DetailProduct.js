68a5472aeddcc9f8c4b9eac77c4faf934fc7a941
jhon.cardenas <jhon.cardenas@wundermanthompson.com>
2022-07-06 15:38:10 -0500

    views ok

--- src/components/detailProducts/DetailProduct.js
+++ src/components/detailProducts/DetailProduct.js
@@ -1,4 +1,7 @@
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import Search from "../search/Search";
+import testImg from "../../assets/images/image.jpeg";
+import "./DetailProduct.scss";
 
 function DetailProducts() {
 
@@ -6,7 +9,38 @@ function DetailProducts() {
 
     return(
         <>
-            <h1>Detail products {params.id} </h1>
+            <Search />
+            <div className="mainContent">
+                <div className="breadCrumb">
+                    <p>
+                        <span>
+                            <Link to="/">span1</Link>
+                        </span>
+                        <span>
+                            <Link to="/">/ span2</Link>
+                        </span>
+                        <span>
+                            <Link to="/">/ span3</Link>
+                        </span>
+                    </p>
+                </div>
+                <div className="boxProduct">
+                    <div className="boxItem">
+                        <div className="img">
+                            <img src={testImg} alt="test" />
+                        </div>
+                        <div className="boxInfo">
+                            <h1 className="info">Lorem ipsum dolor sit amet consectetur adipisicing. {params.id}</h1>
+                            <p className="price">$ 1.264</p>
+                            <button className="buy">Comprar ahora</button>
+                        </div>
+                        <div className="boxDescription">
+                            <p className="title">Descripción</p>
+                            <p className="desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus itaque vel adipisci perferendis quis eveniet minus sequi amet nemo quas, nulla ut ab eaque sapiente labore. Nihil dolores nam autem.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
--- src/components/listProducts/ListProducts.js
+++ src/components/listProducts/ListProducts.js
@@ -1,8 +1,91 @@
+import { Link } from "react-router-dom";
+import "./ListProducts.scss";
+import testImg from "../../assets/images/image.jpeg";
 
 function ListProducts() {
     return(
         <>
-            <h1>List products</h1>
+            <div className="mainContent">
+                <div className="breadCrumb">
+                    <p>
+                        <span>
+                            <Link to="/">span1 </Link>
+                        </span>
+                        <span>
+                            <Link to="/">/ span2 </Link>
+                        </span>
+                        <span>
+                            <Link to="/">/ span3</Link>
+                        </span>
+                    </p>
+                </div>
+                <div className="boxProducts">
+                    <div className="boxItem">
+                        <div className="img">
+                            <Link to="/detail/1">
+                                <img src={testImg} alt="test" />
+                            </Link>
+                        </div>
+                        <div className="boxInfo">
+                            <p className="price">$ 1.264</p>
+                            <Link to="/detail/1">
+                                <h2 className="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi rem vero iure voluptates vitae.</h2>
+                            </Link>
+                        </div>
+                        <div className="boxPromo">
+                            <p className="promo">Promoción</p>
+                        </div>
+                    </div>
+                    <div className="boxItem">
+                        <div className="img">
+                            <Link to="/detail/2">
+                                <img src={testImg} alt="test" />
+                            </Link>
+                        </div>
+                        <div className="boxInfo">
+                            <p className="price">$ 1.264</p>
+                            <Link to="/detail/2">
+                                <h2 className="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi rem vero iure voluptates vitae.</h2>
+                            </Link>
+                        </div>
+                        <div className="boxPromo">
+                            <p className="promo">Promoción</p>
+                        </div>
+                    </div>
+                    <div className="boxItem">
+                        <div className="img">
+                            <Link to="/detail/3">
+                                <img src={testImg} alt="test" />
+                            </Link>
+                        </div>
+                        <div className="boxInfo">
+                            <p className="price">$ 1.264</p>
+                            <Link to="/detail/3">
+                                <h2 className="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi rem vero iure voluptates vitae.</h2>
+                            </Link>
+                        </div>
+                        <div className="boxPromo">
+                            <p className="promo">Promoción</p>
+                        </div>
+                    </div>
+                    <div className="boxItem">
+                        <div className="img">
+                            <Link to="/detail/4">
+                                <img src={testImg} alt="test" />
+                            </Link>
+                        </div>
+                        <div className="boxInfo">
+                            <p className="price">$ 1.264</p>
+                            <Link to="/detail/4">
+                                <h2 className="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi rem vero iure voluptates vitae.</h2>
+                            </Link>
+                        </div>
+                        <div className="boxPromo">
+                            <p className="promo">Promoción</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
